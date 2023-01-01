51f56875ee49bc4ad765f8a4febb45528b8c60c3
hyriss02 <113988964+hyriss02@users.noreply.github.com>
2023-01-01 23:42:41 +0800

    Update config.js

--- config.js
+++ config.js
@@ -17,14 +17,14 @@ global.APIKeys = {
 }
 
 
-global.ownername = 'THE JO BOT'
+global.ownername = 'Cute'
 global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['60177426853','60177426853','60177426853']
-global.premium = ['60177426853']
-global.packname = 'The Jo Bot'
+global.owner = ['6281548721716','6281548721716','6281548721716']
+global.premium = ['6281548721716']
+global.packname = 'cute'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
 global.sessionName = 'session'
